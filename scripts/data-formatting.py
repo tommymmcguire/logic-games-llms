696c61f3231f6f88ac8e6bce19fdf346b6cb63a9
nicholasconterno <nec21@duke.edu>
2024-03-19 13:36:46 -0400

    added data files

---
+++ scripts/data-formatting.py
@@ -0,0 +1,25 @@
+import json
+
+files = ['data/downloaded-data/train.jsonl', 'data/downloaded-data/validation.jsonl', 'data/downloaded-data/test.jsonl']
+
+for file_path in files:
+    litgpt_format_data = []
+
+    # Open the .jsonl file and process each line
+    with open(file_path, 'r') as file:
+        for line in file:
+            example = json.loads(line)  # Parse each line as JSON
+            new_example = {
+                "instruction": example["question"],
+                "input": example["puzzle_text"],
+                "output": example["answer"]+'\n',
+            }
+            litgpt_format_data.append(new_example)
+
+    # Derive the new filename for the formatted data
+    name = file_path.split('/')[-1].split('.')[0]
+    output_file_path = f'data/formatted-data/{name}_data_formatted.json'
+
+    # Dump the converted data to a JSON file
+    with open(output_file_path, 'w') as json_file:
+        json.dump(litgpt_format_data, json_file, indent=4)
